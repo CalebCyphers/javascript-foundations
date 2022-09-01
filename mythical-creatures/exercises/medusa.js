1133e34bb6b3dbdd7a7ef5eed305482547e1506b
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2022-08-31 23:00:11 -0600

    Make a Medus which passes all tests

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,19 @@
-var Person = require('./person');
-var Statue = require('./statue');
+var Person = require("./person");
+var Statue = require("./statue");
+
+class Medusa {
+  constructor(name) {
+    this.name = name;
+    this.statues = [];
+    this.gazeAtVictim = (person) => {
+      var statue = new Statue(person.name);
+      this.statues.push(statue);
+      if (this.statues.length > 3) {
+        var freeStatue = this.statues.shift();
+        return new Person(freeStatue.name, 'relieved')
+      }
+    };
+  }
+}
+
+module.exports = Medusa;
